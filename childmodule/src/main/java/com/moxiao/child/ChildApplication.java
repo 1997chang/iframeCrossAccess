1eafb553fce8d0883fb37d40a57885d7ed8992fc
changxiao <546613470@qq.com>
2021-11-26 09:40:51 +0800

    初始化项目

---
+++ childmodule/src/main/java/com/moxiao/child/ChildApplication.java
@@ -0,0 +1,16 @@
+package com.moxiao.child;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChangWei[changwei@viewshine.cn]
+ */
+@SpringBootApplication
+public class ChildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildApplication.class, args);
+    }
+
+}
---
+++ fathermodule/src/main/java/com/moxiao/father/FatherApplication.java
@@ -0,0 +1,15 @@
+package com.moxiao.father;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChangWei[changwei@viewshine.cn]
+ */
+@SpringBootApplication
+public class FatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatherApplication.class, args);
+    }
+}
